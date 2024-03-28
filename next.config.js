1d5c861ab3da9c28533f34e15d341cf62ae9fcce
yume-ebina <cityeventa@gmail.com>
2024-03-28 09:53:05 +0900

    webpack5に変更

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+};
