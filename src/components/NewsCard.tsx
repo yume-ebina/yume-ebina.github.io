289433620392f325ff69ef0c28491d33f34e42b6
yume-ebina <cityeventa@gmail.com>
2024-04-01 13:36:20 +0900

    NewsCard サイズ調整

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -15,7 +15,7 @@ const NewsCard = ({
 }) => {
   return (
     <Link href={href}>
-      <div className=" h-80 bg-white rounded-lg p-5 mb-5 hover:bg-slate-200 duration-300 md:mr-5">
+      <div className="bg-white rounded-lg p-5 mb-5 hover:bg-slate-200 duration-300 md:mr-5">
         <div className="mb-3 flex justify-center items-center">
           <img src={image} />
         </div>
